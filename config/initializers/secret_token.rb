d5e2ffd1b3ab3077b4ee8de7e1b8b18bab8b2396
Alex Agranov <alex@morphogenic.net>
2011-11-03 02:56:49 -0400

    making the rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EarningsDateFinder::Application.config.secret_token = '285c5772878f47caafd0add4d300a71ac2db02810d40bbe3cbcc79721c774639339860616257e3b316815ca4ba1de6033bdbc8b370f772a71c7f78525e183944'
